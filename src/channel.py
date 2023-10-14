0f336ac704296afa08814977949183f853f32504
ylug <amaterasumyeye@gmail.com>
2023-10-15 00:24:14 +0300

    develop

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,8 @@
+import json
+import os
+
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -5,8 +10,25 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        api_key: str = os.getenv('YT_API_KEY')
+
+        # создать специальный объект для работы с API
+        youtube = build('youtube', 'v3', developerKey=api_key)
+
+        def printj(dict_to_print: dict) -> None:
+            """Выводит словарь в json-подобном удобном формате с отступами"""
+            print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
+        '''
+        получить данные о канале по его id
+        docs: https://developers.google.com/youtube/v3/docs/channels/list
+
+        сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
+        '''
+        channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        printj(channel)
