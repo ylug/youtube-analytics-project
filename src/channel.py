8e1cc7eb1ebcacd8b8a91bf22c6d8dd817c7617c
ylug <amaterasumyeye@gmail.com>
2023-10-15 20:38:17 +0300

    develop

--- src/channel.py
+++ src/channel.py
@@ -19,16 +19,7 @@ class Channel:
         # создать специальный объект для работы с API
         youtube = build('youtube', 'v3', developerKey=api_key)
 
-        def printj(dict_to_print: dict) -> None:
-            """Выводит словарь в json-подобном удобном формате с отступами"""
-            print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
-        '''
-        получить данные о канале по его id
-        docs: https://developers.google.com/youtube/v3/docs/channels/list
-
-        сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
-        '''
-        channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
-        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        printj(channel)
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
