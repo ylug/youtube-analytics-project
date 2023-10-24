d9ee73588d5c60b7be35c5203096c3d0f2b09452
ylug <amaterasumyeye@gmail.com>
2023-10-24 23:19:39 +0300

    develop

--- src/channel.py
+++ src/channel.py
@@ -17,26 +17,68 @@ class Channel:
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.description = self.channel["items"][0]["snippet"]["description"]
         self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
-        self.subscribers_channel = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.subscriberCount = self.channel["items"][0]["statistics"]["subscriberCount"]
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
         self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
-
-    def print_info(self) -> None:
-        """Выводит словарь в json-подобном удобном формате с отступами"""
-        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
-
-    @classmethod
-    def get_service(cls):
-        api_key: str = os.getenv('YT_API_KEY')
-        return build('youtube', 'v3', developerKey=api_key)
 
     def to_json(self, file):
         attrib_dict = {"channel_id": self.__channel_id,
                        "title": self.title,
                        "description": self.description,
                        "url": self.url,
-                       "subscribers_channel": self.subscribers_channel,
+                       "subscribers_channel": self.subscriberCount,
                        "video_count": self.video_count,
                        "view_count": self.view_count}
         with open(file, "w", encoding='utf-8') as f:
             json.dump(attrib_dict, f, indent=2, ensure_ascii=False)
+
+    def __str__(self):
+        return (f"Название канала:{self.title}.\nОписание канала: {self.description}"
+                f" ,\nКоличество подписчиков: {self.subscriberCount},\nКоличество видео на канале: {self.video_count},\n"
+                f"Количество просмотров на канале: {self.view_count}.")
+
+    def __add__(self, other):
+        """Суммарное количество просмотров двух каналов"""
+        subscriberCount_1 = self.subscriberCount
+        subscriberCount_2 = other.subscriberCount
+        return int(subscriberCount_1) + int(subscriberCount_2)
+
+    def __sub__(self, other):
+        """Разница количества просмотров двух каналов"""
+        subscriberCount_1 = self.subscriberCount
+        subscriberCount_2 = other.subscriberCount
+        return int(subscriberCount_1) - int(subscriberCount_2)
+
+    def __eq__(self, other):
+        """Сравнение на равенство"""
+        subscriberCount_1 = self.subscriberCount
+        subscriberCount_2 = other.subscriberCount
+        return int(subscriberCount_1) == int(subscriberCount_2)
+
+    def __lt__(self, other):
+        """Сравнение операторов (меньше)"""
+        subscriberCount_1 = self.subscriberCount
+        subscriberCount_2 = other.subscriberCount
+        return subscriberCount_1 < subscriberCount_2
+
+    def __le__(self, other):
+        """Сравнение операторов (меньше или равно)"""
+        subscriberCount_1 = self.subscriberCount
+        subscriberCount_2 = other.subscriberCount
+        return int(subscriberCount_1) <= int(subscriberCount_2)
+
+    def __gt__(self, other):
+        """Сравнение операторов (больше)"""
+        subscriberCount_1 = self.subscriberCount
+        subscriberCount_2 = other.subscriberCount
+        return int(subscriberCount_1) > int(subscriberCount_2)
+
+    def __ge__(self, other):
+        """Сравнение операторов (больше или равно)"""
+        subscriberCount_1 = self.subscriberCount
+        subscriberCount_2 = other.subscriberCount
+        return int(subscriberCount_1) >= int(subscriberCount_2)
+
+    def print_info(self) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
