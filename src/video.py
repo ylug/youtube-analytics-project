b617a2726514f82e102789575ea28c56b985b951
ylug <amaterasumyeye@gmail.com>
2023-10-28 19:19:01 +0300

    develop

--- src/video.py
+++ src/video.py
@@ -1,40 +1,31 @@
-from googleapiclient.discovery import build
 import os
+from googleapiclient.discovery import build
 
 
 class Video:
-    youtube = build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def __init__(self, video_id):
-        self.video_id = video_id
-        try:
-            video_response = self.youtube.videos().list(part='snippet, statistics, contentDetails, topicDetails',
-                                                        id=self.video_id).execute()
+    def __init__(self, video_id: str) -> None:
+        self._video_id = video_id
+        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                    id=video_id
+                                                    ).execute()
 
-            self.title = video_response['items'][0]['snippet']['title']
-            self.video_link = f"https://youtu.be/{self.video_id}"
-            self.view_count = video_response['items'][0]['statistics']['viewCount']
-            self.like_count = video_response['items'][0]['statistics']['likeCount']
-
-        except IndexError:
-            self.title = None
-            self.video_link = None
-            self.view_count = None
-            self.like_count = None
-
-    def fetch_video_info(self):
-        service = build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
-        video_response = service.videos().list(part='snippet, statistics', id=self.video_id).execute()
-        self.title = video_response['items'][0]['snippet']['title']
-        self.video_link = f"https://youtu.be/{self.video_id}"
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+        self.title = str(video_response['items'][0]['snippet']['title'])
+        self.count = str(video_response['items'][0]['statistics']['viewCount'])
+        self.like_count = int(video_response['items'][0]['statistics']['likeCount'])
+        self.comment_count = int(video_response['items'][0]['statistics']['commentCount'])
 
     def __str__(self):
-        return self.title
+        return f"{self.title}"
 
 
 class PLVideo(Video):
+
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
-        self.playlist_id = playlist_id  # Add the title parameter here
+        self.playlist_id = playlist_id
+
+    def __str__(self):
+        return f"{self.title}"
